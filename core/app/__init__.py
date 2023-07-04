d04361b9d764a1aee4041a4f511dc9890097a6da
ivalykhin <ivalykhin@gmail.com>
2023-07-05 00:39:49 +0700

    fix app

--- core/app/__init__.py
+++ core/app/__init__.py
@@ -15,31 +15,19 @@ from .config import LOGGER_CONFIGURATION_FILE
 from .extensions import db, migrate, bcrypt, metrics, scheduler, auth
 
 from .main.controller.users import users_api
-from .main.controller.nodes import nodes_api
-from .main.controller.user_configs import user_configs_api
-from .main.controller.servers import servers_api
-from .main.controller.subcriptions import subscriptions_api
-from .main.controller.user_subscriptions import user_subscriptions_api
-from .main.controller.invitations import invitations_api
-from .main.controller.locations import locations_api
-from .main.controller.prometheus_targets import prometheus_api
-
-from .main.dto.config import config_dto
-from .main.dto.country import country_dto
-from .main.dto.node import nodes_dto
-from .main.dto.subscription import subscription_dto, subscriptions_dto
-from .main.dto.user import UserDto
-from .main.dto.server import server_dto, servers_dto
-from .main.dto.user_config import user_config_dto, user_configs_dto
-from .main.dto.user_subscription import user_subscription_dto
-from .main.dto.server_invitation import server_invitation_dto
-from .main.dto.prometheus import prometheus_targets_dto
 
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import current_app
 
 from .config import BASIC_AUTH_PROMETHEUS_USER, BASIC_AUTH_PROMETHEUS_PASSWORD, \
     BASIC_AUTH_CLIENT_USER, BASIC_AUTH_CLIENT_PASSWORD, BASIC_AUTH_CORE_USER, BASIC_AUTH_CORE_PASSWORD
+
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
+from common.dto.user import users_dto, user_dto
 
 users = {}
 
@@ -72,16 +60,7 @@ def register_blueprints(app):
     swagger_blueprint.register_blueprint(create_swagger(app))
     swagger_blueprint.register_blueprint(create_swagger_ui(app))
 
-    users_api.register_blueprint(user_configs_api)
-    users_api.register_blueprint(user_subscriptions_api)
-
     blueprint.register_blueprint(users_api)
-    blueprint.register_blueprint(nodes_api)
-    blueprint.register_blueprint(servers_api)
-    blueprint.register_blueprint(subscriptions_api)
-    blueprint.register_blueprint(invitations_api)
-    blueprint.register_blueprint(locations_api)
-    blueprint.register_blueprint(prometheus_api)
 
     app.register_blueprint(blueprint)
     app.register_blueprint(swagger_blueprint)
@@ -125,19 +104,8 @@ def get_apispec(app):
         openapi_version="3.0.2",
         plugins=[FlaskPlugin(), MarshmallowPlugin()],
     )
-    spec.components.schema("NodeList", schema=nodes_dto)
     spec.components.schema("User", schema=UserDto())
     spec.components.schema("UserList", schema=UserDto(many=True))
-    spec.components.schema("Server", schema=server_dto)
-    spec.components.schema("ServerList", schema=servers_dto)
-    spec.components.schema("Config", schema=config_dto)
-    spec.components.schema("Country", schema=config_dto)
-    spec.components.schema("UserConfig", schema=user_config_dto)
-    spec.components.schema("UserConfigList", schema=user_configs_dto)
-    spec.components.schema("Subscription", schema=subscription_dto)
-    spec.components.schema("SubscriptionList", schema=subscriptions_dto)
-    spec.components.schema("UserSubscription", schema=user_subscription_dto)
-    spec.components.schema("PrometheusTargetsList", schema=prometheus_targets_dto)
     load_docstrings(spec, app)
     return spec
 
